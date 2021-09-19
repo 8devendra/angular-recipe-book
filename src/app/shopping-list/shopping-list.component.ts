1407c11e6bd7186b4491dec68a92d56c9c1d7faa
8devendra <devendra6969@gmail.com>
2021-09-19 17:39:49 +0530

    template desine done

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  ingredients:Ingredient[]=[
+    new Ingredient("Dal",40),
+    new Ingredient("Rice",30)
+  ];
   constructor() { }
 
   ngOnInit(): void {
